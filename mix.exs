0f2f60800114ec24f55d67b740da21975e4cdb23
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2023-05-03 12:26:08 -0600

    Add missing files to publish hex package (#2)

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Gamco.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/amco/gamco-ex"
+  @version "0.1.0"
+
   def project do
     [
       app: :gamco,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      source_url: @source_url,
+      homepage_url: @source_url
     ]
   end
 
@@ -23,6 +30,27 @@ defmodule Gamco.MixProject do
     [
       {:jason, ">= 1.4.0"},
       {:phoenix_live_view, ">= 0.17.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      formatters: ["html"],
+      extras: ["CHANGELOG.md", "CONTRIBUTING.md", "README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      description: "Google Analytics wrapper for elixir applications.",
+      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "CONTRIBUTING.md", "LICENSE"],
+      maintainers: ["Alejandro Gutiérrez"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url,
+        Changelog: "https://hexdocs.pm/gamco/changelog.html"
+      }
     ]
   end
 end
