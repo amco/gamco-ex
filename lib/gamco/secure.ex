cd97934c3635d7c2e22086f8168e3ba03d1c7212
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2023-05-02 18:00:59 -0600

    Add tests for gamco secure

--- lib/gamco/secure.ex
+++ lib/gamco/secure.ex
@@ -1,4 +1,20 @@
 defmodule Gamco.Secure do
+  @moduledoc """
+  This module provides the default function to secure
+  any sensitive information in order to be send to GA.
+  """
+
+  @doc """
+  This function hashes the value passed as argument
+  using md5 hexadecimal and downcases the resulting
+  string.
+
+  ## Examples
+
+      iex> Gamco.Secure.call("foo")
+      "acbd18db4cc2f85cedef654fccc4a4d8"
+
+  """
   def call(value) do
     :crypto.hash(:md5 , value)
     |> Base.encode16()
